1a8954f1736126d9560e4ddc196673243c76ac86
Jan Tarnowski <tarnowski.john@gmail.com>
2017-12-02 21:21:39 +0100

    Adds Spring Boot application configuration

---
+++ src/main/java/org/shopping/AppConfiguration.java
@@ -0,0 +1,20 @@
+package org.shopping;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan({"org.shopping.domain.cart", "org.shopping.controller", "org.shopping.infrastructure"})
+class AppConfiguration {
+
+    @Bean
+    ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        return objectMapper;
+    }
+}
